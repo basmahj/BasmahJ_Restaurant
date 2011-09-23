b684c10ba7699cecd359d40fbd2d6c9957ba7a9f
basmahj <basmah.jedai@gmail.com>
2011-09-23 11:01:44 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BasmahJRestaurant::Application.config.secret_token = 'b3c297e3e9e347d4679fdf1775a5c5bee9e6db293cec09a199c85b5f41cb057191d753453fa38810aad26611ad6ecf82403eb7a2381d2071d95d8152ad1b9fd6'
